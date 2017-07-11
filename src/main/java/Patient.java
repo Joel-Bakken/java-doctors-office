26a5a16a31106d94e86c10f1a137be87382b059f
Anna Timofeeva and Joel Bakken <joel@>
2017-07-11 13:28:29 -0700

    add patient name

--- src/main/java/Patient.java
+++ src/main/java/Patient.java
@@ -3,5 +3,13 @@ import java.util.List;
 import org.sql2o.*;
 
 public class Patient {
-  
+  private String name;
+
+  public Patient(String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
 }
--- src/test/java/PatientTest.java
+++ src/test/java/PatientTest.java
@@ -7,4 +7,17 @@ public class PatientTest {
   @Rule
   public DatabaseRule database = new DatabaseRule();
 
+  @Test
+  public void patient_instantiatesCorrectly_true() {
+    Patient testPatient = new Patient("Bob");
+    assertEquals(true, testPatient instanceof Patient);
+  }
+
+  @Test
+  public void patient_instantiatesWithName_bob() {
+    Patient testPatient = new Patient("Bob");
+    assertEquals("Bob", testPatient.getName());
+  }
+
+
 }
